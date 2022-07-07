715dc512121b97d5f62861ff08cb10924b1f7c24
= <bd2845@naver.com>
2022-07-07 10:56:32 +0900

    gui 창 닫을시 아이템 저장

--- src/main/java/com/division/listener/GuiCloseListener.java
+++ src/main/java/com/division/listener/GuiCloseListener.java
@@ -1,15 +1,24 @@
 package com.division.listener;
 
+import com.division.data.RouletteData;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class GuiCloseListener implements Listener {
+
+    private RouletteData data;
 
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
         if (event.getView().getTitle().contains("룰렛 설정")) {
-
+            for (int i = 0; i < 27; i++) {
+                ItemStack stack = event.getInventory().getItem(i);
+                if (stack != null && stack.getType() != Material.AIR)
+                        data.setGuiDataByIndex(stack, i);
+            }
         }
     }
 }
